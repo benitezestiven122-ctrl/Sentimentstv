40f20369a8418fc0a4fc16d1b718190a0bf19c78
benitezestiven122-ctrl <benitezestiven122@gmail.com>
2026-09-10 13:35:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,14 +34,14 @@ with st.expander('Analizar texto'):
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
-            st.write( 'Es un sentimiento Positivo 😊')
-              with open('Happy. json') as source:
-                animation=json. load(source)
-              st. lottie(animation,width =350)
+          st.write( 'Es un sentimiento Positivo 😊')
+          with open('Happy. json') as source:
+            animation=json. load(source)
+          st. lottie(animation,width =350)
         elif x >=-1 and x <= 0:
-            st.write( 'Es un sentimiento Negativo 😔')
-              with open('sad emoticon. json') as source:
-                animation=json. load(source)
-              st. lottie(animation,width =350)
+          st.write( 'Es un sentimiento Negativo 😔')
+            with open('sad emoticon. json') as source:
+              animation=json. load(source)
+            st. lottie(animation,width =350)
         else:
             st.write( 'Es un sentimiento Neutral 😐')
